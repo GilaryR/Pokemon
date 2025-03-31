db5f9b9b48523abe3a461d2fbb7f5e8a53b87196
Alejandra Ortega <alejandrav.ortegad@autonoma.edu.co>
2025-03-31 17:37:20 -0500

    clases

--- src/autonoma/Pokemon/models/Squirtle.java
+++ src/autonoma/Pokemon/models/Squirtle.java
@@ -1,8 +1,24 @@
-
 package autonoma.Pokemon.models;
 
-
+/**
+ * @author Alejandra
+ * @since 2025/03/31
+ * @version 1.0
+ */
 public class Squirtle implements Pokemon, PokemonAgua {
+    @Override
+    public void atacar() {
+        System.out.println("Squirtle usa Pistola de Agua!");
+    }
 
+    @Override
+    public void defender() {
+        System.out.println("Squirtle se esconde en su caparazón.");
+    }
+
+    @Override
+    public void atacarHidrobomba() {
+        System.out.println("Squirtle lanza un chorro de agua a alta presión!");
+    }
 }
 
